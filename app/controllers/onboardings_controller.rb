83d02a1e62bf127d273e3ca5f00f4c61742b9d2a
Corentin Ripaud <corentin.ripaud@gmail.com>
2024-03-06 15:20:08 +0100

    user sign in set up

--- app/controllers/onboardings_controller.rb
+++ app/controllers/onboardings_controller.rb
@@ -1,15 +1,16 @@
 class OnboardingsController < ApplicationController
   def edit
-    # @onboarding = Onboarding.new
     @user = current_user
   end
 
   def update
+    @user = current_user
+    @user.update(onboarding_params)
   end
 
   private
 
   def onboarding_params
-    params.require(:onboarding).permit({})
+    params.require(:onboarding).permit(:first_name, :last_name, :sexe, :size, :weight, :goal, :training, :body_fat)
   end
 end
