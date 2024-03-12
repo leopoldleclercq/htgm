b8d3cf276c60392dfd7c9e321367d91259aa7f53
léopold leclercq <leopold.leclercq@audencia.com>
2024-03-12 17:05:28 +0100

    push

--- app/javascript/controllers/image_switch_controller.js
+++ app/javascript/controllers/image_switch_controller.js
@@ -4,8 +4,6 @@ export default class extends Controller {
   static targets = ["image"]
 
   switch() {
-    const imageName = this.imageTarget.src;
-    const newImage = imageName.includes("unpeumuscle") ? "tresmuscle.png" : "unpeumuscle.png";
-    this.imageTarget.src = `/assets/${newImage}`;
+    this.imageTarget.src = `/assets/tresmuscle-d6412823b975e7472ff9714299c516f86a04326dee00b36159928c0ac1d53022.png`;
   }
 }
